cf3d08ac61fbd9ee446baff8933b71cb23e489b0
Brian Ketelsen <bketelsen@gmail.com>
2022-05-29 03:26:31 -0400

    first post

---
+++ components/meta/siteDefaults.ts
@@ -0,0 +1,8 @@
+// Defaults for meta, if not configured in CMS
+export const siteTitleMeta = 'brian.dev | Brian Ketelsen'
+export const siteDescriptionMeta = 'Brian Ketelsen is a developer advocate, keynote speaker, author, and open source practitioner.'
+
+// Defaults, if not configured in CMS
+// Images can be fund in the /public folder
+export const siteIcon = 'favicon.png'
+export const siteImage = 'site-meta.png'
